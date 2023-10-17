c58b52eac7bf1916e2f0599928df8bde83d33c8d
saahhh <143372306+saahhh@users.noreply.github.com>
2023-10-17 17:08:23 +0900

    db_oracle

--- chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
+++ chap1_DB_ORACLE/src/com/kh/db/oraclesample/DBConnection.java
@@ -1,6 +1,7 @@
 package com.kh.db.oraclesample;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,6 +10,12 @@ import java.sql.SQLException;
 public class DBConnection {
 
 	public static void main(String[] args) {
+//		selectBank();
+//		selectkhcafe();
+		selectIf();
+	}
+		
+	static void selectBank() {
 		//1. 드라이버 연결 : Oracle JDBC 드라이버 클래스 이름
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		//2. 오라클 내 컴퓨터 연결 : 데이터베이스 연결 정보
@@ -23,7 +30,7 @@ public class DBConnection {
 			System.out.println("데이터베이스 연결 성공!");
 			
 			//SELECT 쿼리
-			String selectQuery = "SELECT * FROM bank";
+			String selectQuery = "SELECT * FROM bank ORDER BY 1";
 			PreparedStatement selectState = con.prepareStatement(selectQuery);
 			ResultSet result = selectState.executeQuery(); //ResultSet result = 결과들의 집합
 			//result.next() : result 객체에서 다음 행(row)로 이동
@@ -38,11 +45,13 @@ public class DBConnection {
 				//2. 함께 해보기 : branchName
 				String branchName = result.getString("branch_name");
 				//3. java.sql import Date lastTransactionDate 가져오기
-				String lastTransactionDate = result.getString("last_transaction_date");
+				Date lastTransactionDate = result.getDate("last_transaction_date");
+				
 				System.out.println("ACCOUNT_ID : " + accountID + ", ACCOUNT_NUMBER : " + accountNumber + ", ACCOUNT_NAME : " + accountName + 
-						", BALANCE : " + balance +", BRANCH_NAME : " + branchName + ", LAST_TRANSACTION_DATE : " + lastTransactionDate);
-
+						", BALANCE : " + balance + ", BRANCH_NAME : " + branchName + ", LAST_TRANSACTION_DATE : " + lastTransactionDate);
+				
 			}
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,4 +60,131 @@ public class DBConnection {
 
 	}
 
+	
+	static void selectkhcafe() {
+		//1. 드라이버 연결 : Oracle JDBC 드라이버 클래스 이름
+				String driver = "oracle.jdbc.driver.OracleDriver";
+				//2. 오라클 내 컴퓨터 연결 : 데이터베이스 연결 정보
+				//								나의IP주소:port번호
+				String url = "jdbc:oracle:thin:@localhost:1521:xe";
+				String user = "khcafe";
+				String password = "kh1234";
+				Connection con = null;
+				//select * from menu 작성해서 menu table 가져오기
+				try {
+					con = DriverManager.getConnection(url, user, password);
+					System.out.println("데이터베이스 연결 성공");
+					String selectQuery = "SELECT * FROM cafes ORDER BY 1";
+					PreparedStatement selectState = con.prepareStatement(selectQuery);
+					ResultSet result  = selectState.executeQuery();
+					while(result.next()) {
+						int cafe_id = result.getInt("CAFE_ID");
+						String cname = result.getString("CNAME");
+						String address = result.getString("ADDRESS");
+						String phone_number = result.getString("PHONE_NUMBER");
+						String operating_hours = result.getNString("OPERATING_HOURS");
+						System.out.println("CAFE_ID : " + cafe_id + ", CAFENAME : " + cname + ", ADDRESS : " + address + ",PHONE_NUMBER : " + phone_number + ", OPERATING_HOURS : " + operating_hours);
+					
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					con = DriverManager.getConnection(url, user, password);
+					String selectQuery = "SELECT * FROM menu";
+					PreparedStatement selectState = con.prepareStatement(selectQuery);
+					ResultSet result = selectState.executeQuery();
+						int menu_id = result.getInt("MENU_ID");
+						int cafe_id = result.getInt("CAFE_ID");
+						String menu_name = result.getString("MENU_NAME");
+						double price = result.getDouble("PRICE");
+						String description = result.getString("DESCRIPTION");
+						System.out.println("MENU_ID : " + menu_id + ", CAFE_ID : " + cafe_id + ", MENU_NAME : " + menu_name + ", PRICE : " + price + ", DESCRIPTION : " + description);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+		}
+	
+	
+	static void selectIf() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; //thin이 driver의 역할을 하기 때문에 String driver = "oracle.jdbc.driver.OracleDriver";가 없어도 되는 것
+		String user = "khbank";
+		String password = "kh1234";
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			//WHERE절 사용해서 조건 추가 //?를 넣어 target코드에서 숫자 바꾸도록
+//			String selectQuery = "SELECT * FROM BANK WHERE account_id = ?";
+//			String selectQuery = "SELECT * FROM BANK WHERE account_name = ?";
+//			String selectQuery = "SELECT * FROM BANK WHERE account_number in (?,?,?)"; 
+//			String selectQuery = "SELECT * FROM BANK WHERE branch_name = ?";
+			String selectQuery = "SELECT * FROM BANK WHERE account_id in (?,?,?)";
+			
+			PreparedStatement selectState = con.prepareStatement(selectQuery);
+			
+			//여기에 원하는 조건의 account_id 설정
+//			int targetAID = 1;
+//			String targetAN = "이지은";
+//			int targetAN =1236777888;
+//			String[] targetAN = {"1234567890","5555666777","3333444555"};
+//			String[] targetBN = {"북쪽지점"};
+			int[] targetAID = {1,2,3};
+			
+			
+			//조건 설정
+//			selectState.setInt(1, targetAID); //1은 자리값
+//			selectState.setString(1, targetAN);
+//			selectState.setInt(1, targetAN);
+
+//			selectState.setString(1, targetAN[0]); 
+//			selectState.setString(2, targetAN[1]);
+//			selectState.setString(3, targetAN[2]); //인덱스 규칙 위반하지 않게 순서 맞춰서 써야함
+			
+			selectState.setInt(1, targetAID[0]);
+			selectState.setInt(2, targetAID[1]);
+			selectState.setInt(3, targetAID[2]);
+			
+			ResultSet result = selectState.executeQuery();
+			
+			//값 존재 여부
+			if(!result.isBeforeFirst()) {
+				System.out.println("존재하는 데이터가 없습니다.");
+			}
+			while (result.next()) {
+				int a = result.getInt("account_id");
+				String b = result.getString("account_number");
+				String c = result.getString("account_name");
+				double d = result.getDouble("balance");
+				String e = result.getString("branch_name");
+				Date f = result.getDate("last_transaction_date");
+				System.out.println("ACCOUNT_ID : " + a + " ACCOUNT_NUMBER : " + b + " ACCOUNT_NAME : " + c + " BALANCE : " + d + 
+						" BRANCH_NAME : " + e + " LAST_TRANSACTION_DATE : " + f);
+			}
+			if(!result.isBeforeFirst()) {
+				System.out.println("존재하는 데이터가 없음"); //while문이 끝난 상태여서 출력되는 것이 당연하기 때문에 while 시작전에 넣어줘야한다.
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 }
+
+//
+//else {
+//	System.out.println("조건에 해당하는 데이터가 없습니다.");
+//}
+
+/*
+String a = result.getString("열이름");
+char[] charArray = 열이름.toCharArray();
+for(char c : charArray) {
+}
+*/
